c68464ae5c3e1f6d22d7314be78565c50a132592
LeSinge <zhesofifi@gmail.com>
2026-02-15 17:32:42 +0300

    Update needs.js

--- needs.js
+++ needs.js
@@ -1,14 +1,8 @@
 export function updateNeeds(p){
-
  p.hunger+=3;
  p.energy-=2;
  p.boredom+=4;
  p.loneliness+=2;
-
- if(p.hunger>100)p.hunger=100;
- if(p.energy<0)p.energy=0;
- if(p.boredom>100)p.boredom=100;
- if(p.loneliness>100)p.loneliness=100;
 }
 
 export function getMood(p){
