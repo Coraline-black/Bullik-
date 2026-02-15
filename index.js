c554c40190f9e4ad8bb0c1f79580a2ad63498b7c
LeSinge <zhesofifi@gmail.com>
2026-02-15 17:21:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
-import { seePerson, pet, ignore } from "./brain.js";
+import { setEmotion } from "./emotions.js";
+import { updateFace } from "./face.js";
+import { updateNeeds, getMood } from "./needs.js";
+import { rememberPerson, getTrust } from "./memory.js";
 
-// делаем функции доступными из консоли и кнопок
-window.seePerson = seePerson;
-window.pet = pet;
-window.ignore = ignore;
+const API="https://bullik.damp-glade-283e.workers.dev/";
 
-// стартовое состояние питомца
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Pet AI started");
+window.seePerson = async(name)=>{
 
-    // начальная эмоция
-    seePerson("stranger");
+    rememberPerson(name);
 
-    // автоматическое поведение
-    setInterval(()=>{
-        const moods=["neutral","curious","sleepy"];
-        const mood=moods[Math.floor(Math.random()*moods.length)];
-        window.dispatchEvent(new CustomEvent("mood",{detail:mood}));
-    },10000);
-});
+    try{
+        const res=await fetch(API+"?name="+name);
+        const data=await res.json();
+
+        let trust=getTrust(name);
+
+        if(trust>70) setEmotion("love");
+        else if(trust>40) setEmotion("happy");
+        else if(trust>10) setEmotion("curious");
+        else setEmotion("scared");
+
+        updateFace(data.emotion);
+
+    }catch{
+        setEmotion("confused");
+        updateFace("confused");
+    }
+};
+
+window.pet=()=>{
+    setEmotion("love");
+    updateFace("love");
+};
+
+window.feed=()=>{
+    window.petState.hunger=0;
+    setEmotion("happy");
+    updateFace("happy");
+};
+
+window.ignore=()=>{
+    setEmotion("sad");
+    updateFace("sad");
+};
+
+window.petState={
+ hunger:20,
+ energy:80,
+ boredom:10,
+ loneliness:0
+};
+
+setInterval(()=>{
+ updateNeeds(window.petState);
+ setEmotion(getMood(window.petState));
+ updateFace(getMood(window.petState));
+},6000);
