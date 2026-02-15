00844025724d0265520faed98779e913e8ec76cd
LeSinge <zhesofifi@gmail.com>
2026-02-15 17:23:32 +0300

    Update face.js

--- face.js
+++ face.js
@@ -7,8 +7,8 @@ const mouth=document.getElementById("mouth");
 const eyes=[e1,e2];
 
 eyes.forEach(e=>{
-    e.style.height="25px";
-    e.style.background="white";
+ e.style.height="25px";
+ e.style.background="white";
 });
 
 mouth.style.borderRadius="0";
@@ -16,39 +16,36 @@ mouth.style.borderRadius="0";
 switch(emotion){
 
 case "happy":
-    mouth.style.borderRadius="0 0 50px 50px";
+ mouth.style.borderRadius="0 0 50px 50px";
 break;
 
 case "sad":
-    mouth.style.borderRadius="50px 50px 0 0";
+ mouth.style.borderRadius="50px 50px 0 0";
 break;
 
 case "scared":
-    eyes.forEach(e=>e.style.height="45px");
+ eyes.forEach(e=>e.style.height="50px");
 break;
 
 case "love":
-    eyes.forEach(e=>e.style.background="pink");
+ eyes.forEach(e=>e.style.background="pink");
 break;
 
-case "excited":
-    eyes.forEach(e=>e.style.background="cyan");
+case "angry":
+ eyes.forEach(e=>e.style.background="red");
 break;
 
 case "sleepy":
-    eyes.forEach(e=>e.style.height="6px");
+ eyes.forEach(e=>e.style.height="5px");
 break;
 
-case "curious":
-    eyes.forEach(e=>e.style.background="yellow");
+case "hungry":
+ mouth.style.borderRadius="50%";
 break;
 
 case "confused":
-    eyes[0].style.background="red";
-    eyes[1].style.background="blue";
+ eyes[0].style.background="blue";
+ eyes[1].style.background="green";
 break;
-
-default:
-    mouth.style.borderRadius="0";
 }
 }
